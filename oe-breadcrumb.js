8ba211d918634a72cce16de261532be5d9c3d5d4
Sumeet <sumeet.kumar@edgeverve.com>
2019-07-31 19:13:15 +0530

    updated oe-breadcrumb.js

--- oe-breadcrumb.js
+++ oe-breadcrumb.js
@@ -6,7 +6,6 @@ import "@polymer/paper-item/paper-item.js";
 import "@polymer/paper-listbox/paper-listbox.js";
 import "@polymer/paper-menu-button/paper-menu-button.js";
 
-
 /**
  * `oe-breadcrumb` component can be used as a navigation aid in user interfaces and it has been categorised in 2 ways:
  * 
@@ -31,13 +30,11 @@ import "@polymer/paper-menu-button/paper-menu-button.js";
  * @demo demo/demo-oe-breadcrumbs.html
  */
 class OeBreadcrumb extends OECommonMixin(PolymerElement) {
-
   /**
    * Fired when an item from the breadcrumb is clicked
    * @event oe-breadcrumb-selected
-   * @param {object} data object with values of label, path and full path on selection 
+   * @param {object} data object with values of label, path and full path on selection
    */
-
 
   static get template() {
     return html`
@@ -56,23 +53,32 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
           @apply --breadcrumb-item;
         }
         .breadcrumb li:hover {
-          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255,1));
-          background-color: var(--oe-breadcrumb-hover-color, rgb(0, 107, 255,0.3));
+          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255, 1));
+          background-color: var(
+            --oe-breadcrumb-hover-color,
+            rgb(0, 107, 255, 0.3)
+          );
           @apply --breadcrumb-item-hover;
         }
         .breadcrumb paper-item:hover {
           cursor: pointer;
-          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255,1));
-          background-color: var(--oe-breadcrumb-hover-color, rgb(0, 107, 255,0.3));
+          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255, 1));
+          background-color: var(
+            --oe-breadcrumb-hover-color,
+            rgb(0, 107, 255, 0.3)
+          );
           @apply --breadcrumb-paper-item-hover;
         }
         #last-data {
-          color: var(--oe-breadcrumb-lastData-color,#808080);
+          color: var(--oe-breadcrumb-lastData-color, #808080);
           @apply --breadcrumb-item;
         }
-        #last-data:hover{
-          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255,1));
-          background-color:var(--oe-breadcrumb-hover-color, rgb(0, 107, 255,0.3));
+        #last-data:hover {
+          color: var(--oe-breadcrumb-hover-text-color, rgb(0, 107, 255, 1));
+          background-color: var(
+            --oe-breadcrumb-hover-color,
+            rgb(0, 107, 255, 0.3)
+          );
           @apply --breadcrumb-item-hover;
         }
         .breadcrumb iron-icon {
@@ -87,25 +93,34 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
       </style>
 
       <ul class="breadcrumb">
-      <template is="dom-if" if="{{_evaluateFirstItem}}">
-        <li on-tap="_tapActionFirstItem">[[_firstItem.label]]</li> / 
-      </template>
-          <template is="dom-if" if="{{_isOverflow}}">
+        <template is="dom-if" if="{{_evaluateFirstItem}}">
+          <li on-tap="_tapActionFirstItem">[[_firstItem.label]]</li>
+          /
+        </template>
+        <template is="dom-if" if="{{_isOverflow}}">
           <li>
-          <paper-menu-button>
-            <iron-icon icon="more-horiz" slot="dropdown-trigger"></iron-icon>
-            <paper-listbox slot="dropdown-content">
-              <template is="dom-repeat" id="middleElementList" items="{{_middleItems}}">
-                <div class="OverflowSection">
-                  <paper-item on-tap="_tapActionOnOverflow" raised>{{item.label}}</paper-item>
-                </div>
-              </template>
-            </paper-listbox>
-          </paper-menu-button>
-          </li> / 
+            <paper-menu-button>
+              <iron-icon icon="more-horiz" slot="dropdown-trigger"></iron-icon>
+              <paper-listbox slot="dropdown-content">
+                <template
+                  is="dom-repeat"
+                  id="middleElementList"
+                  items="{{_middleItems}}"
+                >
+                  <div class="OverflowSection">
+                    <paper-item on-tap="_tapActionOnOverflow" raised
+                      >{{item.label}}</paper-item
+                    >
+                  </div>
+                </template>
+              </paper-listbox>
+            </paper-menu-button>
+          </li>
+          /
         </template>
         <template is="dom-if" if="{{_evaluateMidItem}}">
-        <li on-tap="_tapActionMidItem">[[_midItem.label]]</li> /
+          <li on-tap="_tapActionMidItem">[[_midItem.label]]</li>
+          /
         </template>
         <li id="last-data" on-tap="_tapActionLastItem">[[_lastItem.label]]</li>
       </ul>
@@ -114,14 +129,13 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
 
   static get properties() {
     return {
-
       /*Array of navigation objects with label and path strings*/
       list: {
         type: Array,
-        value: function () {
+        value: function() {
           return [];
         },
-        observer: '_computeList'
+        observer: "_computeList"
       }
     };
   }
@@ -130,31 +144,32 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
    * Function that checks the lengthlengthlength of list and finds first, midle and last elemnts from a list
    */
   _computeList() {
-
-    if (typeof this.list !== 'undefined' && this.list) {
+    if (typeof this.list !== "undefined" && Array.isArray(this.list)) {
       var bList = this.list;
       var arrayLength = bList.length;
-      var _firstItem = '';
-      var _lastItem = '';
-      var _midItem = '';
+      var _firstItem = null;
+      var _lastItem = null;
+      var _midItem = null;
 
       this.set("arrayLength", arrayLength);
 
-      if (bList.length > 1)
-        this.set("_evaluateFirstItem", true);
+      if (bList.length > 1) this.set("_evaluateFirstItem", true);
       if (bList.length > 3) this.set("_isOverflow", true);
       if (bList.length === 3) this.set("_evaluateMidItem", true);
 
-      if (typeof bList[0] !== 'undefined' && bList[0] != null)
+      if (typeof bList[0] !== "undefined" && bList[0] !== null) {
         _firstItem = bList[0];
+      }
       this.set("_firstItem", _firstItem);
 
-      if (typeof bList[bList.length - 1] !== 'undefined' && bList[bList.length - 1] != null)
+      if (typeof bList[bList.length - 1] !== "undefined" && bList[bList.length - 1] !== null) {
         _lastItem = bList[bList.length - 1];
+      }
       this.set("_lastItem", _lastItem);
 
-      if (typeof bList[bList.length - 2] !== 'undefined' && bList[bList.length - 2] != null)
+      if (typeof bList[bList.length - 2] !== "undefined" && bList[bList.length - 2] !== null) {
         _midItem = bList[bList.length - 2];
+      }
       this.set("_midItem", _midItem);
 
       var _middleItems = bList.slice(1, -1);
@@ -162,14 +177,12 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
     }
   }
 
-
   /**
    * Fires oe-breadcrumb-selected event on selection of any item from the dropdown menu
-   * 
-   *  
+   *
+   *
    */
   _tapActionOnOverflow(e) {
-
     var index = e.model.index + 1;
     var fullPath = "";
     var label = e.model.item.label;
@@ -188,7 +201,6 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
    * Fires oe-breadcrumb-selected event on selection of first item
    */
   _tapActionFirstItem() {
-
     var label = this._firstItem.label;
     var path = this._firstItem.path;
     var fullPath = "";
@@ -203,7 +215,6 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
    * Fires oe-breadcrumb-selected event on selection of middle item
    */
   _tapActionMidItem(e) {
-
     var index = this.arrayLength - 2;
     var label = this._midItem.label;
     var path = this._midItem.path;
@@ -220,10 +231,9 @@ class OeBreadcrumb extends OECommonMixin(PolymerElement) {
 
   /**
    *  Fires oe-breadcrumb-selected event on selection of last item
-   * 
+   *
    */
   _tapActionLastItem() {
-    
     var index = this.arrayLength - 1;
     var fullPath = "";
     var label = this._lastItem.label;
